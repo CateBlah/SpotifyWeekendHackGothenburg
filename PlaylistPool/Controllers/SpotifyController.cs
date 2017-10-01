0ae3df725b6c52ca2ba8e9a36d82095093f10089
oliver <olive@DESKTOP-76C8PP7>
2017-10-01 13:51:32 +0200

    removed shit

--- PlaylistPool/Controllers/SpotifyController.cs
+++ PlaylistPool/Controllers/SpotifyController.cs
@@ -84,23 +84,5 @@ namespace PlaylistPool.Controllers
 
             return returnUsers;
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
